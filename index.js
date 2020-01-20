d8f192fcb4135a257eb24fd2ac8a5d5bfae4d82f
zabrivera <zabdiel.dev@gmail.com>
2020-01-19 23:04:12 -0500

    First Message

---
+++ index.js
@@ -0,0 +1,19 @@
+function updateObjectWithKeyAndValue(obj, key, val){
+  return Object.assign({}, obj, {[key]: val})
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, val){
+  obj[key] = val
+  return obj
+}
+
+function deleteFromObjectByKey(obj, key){
+  var newObj = Object.assign({}, obj)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key){
+  delete obj[key]
+  return obj
+}
